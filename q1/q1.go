d5c5916d226b67f50c384ada3283d7a6dce74601
Kaynan <125670822+KaynanSouza@users.noreply.github.com>
2023-04-21 08:14:47 -0300

    resp 1

--- q1/q1.go
+++ q1/q1.go
@@ -1,6 +1,26 @@
-package q1
+package main
+
+import "fmt"
 
 func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	var resp, soma, media float64
+	currentPurchase = 1000
+	purchaseHistory = []float64{}
+	for i := 0; i < len(purchaseHistory); i++ {
+		soma += purchaseHistory[i]
+	}
+	media = soma / float64(len(purchaseHistory))
+	if media > 1000 {
+		resp = currentPurchase * 0.80
+	} else if purchaseHistory[0] == 0 {
+		resp = currentPurchase * 0.90
+	} else if currentPurchase > 1000 {
+		resp = currentPurchase * 0.90
+	} else if currentPurchase <= 1000 && currentPurchase > 500 {
+		resp = currentPurchase * 0.95
+	} else if currentPurchase <= 500 {
+		resp = currentPurchase * 0.98
+	}
+	fmt.Printf("%f", resp)
+	return resp, nil
 }
